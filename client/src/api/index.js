d21d36443b9626a72ddc4a60280ad386b360e82a
bouyo madaliou <bouyomadaliou@gmail.com>
2021-07-05 21:19:45 +0000

    change login url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({ baseURL: 'http://188.165.235.13:4000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
