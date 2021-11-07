dd001c89ae84208aa0aa70f13ea871a3cb1c58fc
BOUYO Madaliou <bouyomadaliou@gmail.com>
2021-11-07 23:26:45 +0000

    calculate duration

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const categoryManager = require('../managers/category');
+const taskManager = require('../managers/task');
+const moment = require('moment');
+
+const getHoursMinutesFormat = minutes => moment.utc(moment.duration(minutes*60, "seconds").asMilliseconds()).format("HH:mm");
+
+
 var allCategories = [];
 
 function getChildrenRecursive(arr) {
@@ -33,6 +39,22 @@ function getChildrenRecursive(arr) {
     return result;
 }
 
+function getChildrenIdsRecursive(arr) {
+    let result = "";
+    for(let i = 0; i < arr.length; i++) {
+        let current = arr[i];
+        let children = allCategories.filter(x => x.parentCategoryId == current._id);
+        if(children.length === 0) {
+            result += `${current._id}|`;
+        } else {
+            result += `${current._id}|`;
+            result += getChildrenIdsRecursive(children);
+        }
+    }
+
+    return result;
+}
+
 function getParentsRecursiveLabels(category) {
     let result = "";
     let parent = allCategories.find(x => category.parentCategoryId == x._id);
@@ -60,6 +82,42 @@ router.post('/all', async (req, res) => {
         const keyword = req.body.keyword || ``;
         allCategories = await categoryManager.list(keyword);
         let result = rearrange();
+        return res.status(200).send(result);
+    } catch (ex) {
+        return res.status(500).send(ex.message);
+    }
+});
+
+
+router.post('/totalDuration', async (req, res) => {
+    
+    try {
+        const { categoryId, beginDate, endDate } = req.body;
+        allCategories = await categoryManager.list("");
+        const category = await categoryManager.get(categoryId);
+        const childrenIdsStr = getChildrenIdsRecursive([category]);
+        const childrenIds = childrenIdsStr.split("|").filter(x => x.length === 24);
+        let t = await taskManager.list3(childrenIds, beginDate, endDate);
+        console.log('toto : ', t);
+        
+        t = t.map(x => {
+            return {
+                _id: x._id,
+                categoryId: x.categoryId,
+                title: `${x.label} | ${moment(x.startedAt).format('HH:mm')}-${moment(x.finishedAt).format('HH:mm')} | ${getHoursMinutesFormat(x.duration)}`,
+                start: moment(x.startedAt).toDate(),
+                end: moment(x.finishedAt).toDate(),
+               
+                description: x.description,
+                duration: x.duration
+            };
+        });
+
+        const result = {
+            tasks: t,
+            duration: t.reduce((a, b) => a + b.duration, 0)
+        };
+
         return res.status(200).send(result);
     } catch (ex) {
         return res.status(500).send(ex.message);
@@ -135,5 +193,7 @@ router.delete('/:id', async (req, res) => {
         return res.status(500).send(ex.message);
     }
 });
+
+
 
 module.exports = router;
